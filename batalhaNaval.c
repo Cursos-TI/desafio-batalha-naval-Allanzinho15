781af0a7569cccffd49e83bf4ebef80f2ae97ebb
Allan Dellon <allanhot2016@gmail.com>
2025-08-22 17:51:24 +0000

    codigo final do batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,144 @@
 #include <stdio.h>
+#define linhas 15
+#define colunas 15
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+void habilidadeDoCone(int MatrizDoCone[linhas][colunas])
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            MatrizDoCone[i][j] = 2;
+        }
+    }
+}
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+void habilidadeDoCruz(int MatrizDoCruz[linhas][colunas])
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            MatrizDoCruz[i][j] = 2;
+        }
+    }
+}
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+void habilidadeDoOctaedro(int MatrizDoOctaedro[linhas][colunas])
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
+            MatrizDoOctaedro[i][j] = 2;
+        }
+    }
+}
+void InicializaçãoDaMatriz(int iniciarmatriz[linhas][colunas], int vetordosnumero[linhas])
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        vetordosnumero[i];
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+        for (int j = 0; j < colunas; j++)
+        {
+            iniciarmatriz[i][j] = 0;
+        }
+    }
+}
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+void logicaDojogo(int numeroQueRepresentaNavio, int matriz[linhas][colunas], int matrizdaHabilidadeCone[linhas][colunas], int matrizdaHabilidadeOctaedro[linhas][colunas], int matrizdaHabilidadeCruz[linhas][colunas])
+{
+    for (int i = 0; i < linhas; i++)
+    {
+        for (int j = 0; j < colunas; j++)
+        {
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+            // navil na
+            if ((i == 2 && j == 3) || (i == 2 && j == 4) || (i == 2 && j == 5))
+            {
+                matriz[i][j] = numeroQueRepresentaNavio;
+            }
+            else if ((i == 5 && j == 7) || (i == 6 && j == 7) || (i == 7 && j == 7))
+            {
+                matriz[i][j] = numeroQueRepresentaNavio;
+            }
+            // Navil na diagonal
+            else if ((i == 2 && j == 9) || (i == 1 && j == 8) || (i == 0 && j == 7))
+            {
+                matriz[i][j] = numeroQueRepresentaNavio;
+            }
+            else if ((i == 7 && j == 0) || (i == 6 && j == 1) || (i == 5 && j == 2))
+            {
+                matriz[i][j] = numeroQueRepresentaNavio;
+            }
+            //habilidades
+            else if ((i == 11 && j < 5) || (i == 10 && j > 0 && j < 4) || (i == 9 && j > 1 && j < 3))
+            {
+                matriz[i][j] = matrizdaHabilidadeCone[i][j];
+            }
+            else if ((i == 11 && j > 9) || (i == 10 && j > 10 && j < 14) || (i == 9 && j > 11 && j < 13) || (i == 12 && j > 10 && j < 14) || (i == 13 && j > 11 && j < 13))
+            {
+                matriz[i][j] = matrizdaHabilidadeOctaedro[i][j];
+            }
+            else if ((i == 5 && j > 8 && j < 14) || (i == 4 && j > 10 && j < 12) || (i == 3 && j > 10 && j < 12) || (i == 6 && j > 10 && j < 12) || (i == 7 && j > 10 && j < 12))
+            {
+                matriz[i][j] = matrizdaHabilidadeCruz[i][j];
+            }
+        }
+    }
+}
+
+void ExibicaoDaEstruturaDoJogo(char vetor1[linhas], int vetor2[linhas],int matriz[linhas][colunas])
+{
+    printf("    ");
+    for (int i = 0; i < linhas; i++)
+    {
+        printf("%c ", vetor1[i]);
+    }
+
+    printf("\n ");
+
+    for (int i = 0; i < linhas; i++)
+    {
+        if (i < 9)
+        {
+            printf(" %d", vetor2[i]);
+            printf(" ");
+        }
+        else
+        {
+            printf("%d ", vetor2[i]);
+        }
+
+        for (int j = 0; j < colunas; j++)
+        {
+
+            printf("%d ", matriz[i][j]);
+        }
+
+        printf("\n ");
+
+    }
+}
+int main()
+{
+    int matriz[linhas][colunas];
+    char vetor1[linhas] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',};
+    int vetor2[linhas] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int navio = 3;
+
+    int matrizdaHabilidadeCone[linhas][colunas];
+    int matrizdaHabilidadeCruz[linhas][colunas];
+    int matrizdaHabilidadeOctaedro[linhas][colunas];
+
+    habilidadeDoCone(matrizdaHabilidadeCone);
+    habilidadeDoCruz(matrizdaHabilidadeCruz);
+    habilidadeDoOctaedro(matrizdaHabilidadeOctaedro);
+
+    InicializaçãoDaMatriz(matriz, vetor2);
+    logicaDojogo(navio, matriz, matrizdaHabilidadeCone, matrizdaHabilidadeOctaedro, matrizdaHabilidadeCruz);
+    ExibicaoDaEstruturaDoJogo(vetor1, vetor2, matriz);
 
     return 0;
 }
